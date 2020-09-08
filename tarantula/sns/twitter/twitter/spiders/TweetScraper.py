66d4153ad312b431cd8da17a12f7155f1e36dec9
duckino <25713361+duckino@users.noreply.github.com>
2020-09-08 15:49:38 -0500

    init scrapy

---
+++ tarantula/sns/twitter/twitter/spiders/TweetScraper.py
@@ -0,0 +1,12 @@
+import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+
+
+class TweetScraper(CrawlSpider):
+    name = 'TweetScraper'
+    allow_domains = ['twitter.com']
+
+    def __init__(self, *a, **kw):
+        super().__init__(*a, **kw)
+
---
+++ tarantula/tests/youtube_test.py
@@ -0,0 +1,8 @@
+# from tarantula.video.youtube import YouTube
+# import os
+# import shutil
+
+# if __name__ == "__main__":
+#     yt = YouTube(input_file = "./jobs/videoinlist.txt", options = {"vlist": True})
+#     yt.download_video_list()
+#     shutil.make_archive("test_video", 'zip', f"{os.getcwd()}")
